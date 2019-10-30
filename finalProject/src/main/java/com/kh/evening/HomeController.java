02ede79cb1f0dd74a70d6ab98290c7741955a051
user2 <user2@KH_G>
2019-10-30 17:41:48 +0900

    a

--- finalProject/src/main/java/com/kh/evening/HomeController.java
+++ finalProject/src/main/java/com/kh/evening/HomeController.java
@@ -32,7 +32,7 @@ public class HomeController {
 //		String formattedDate = dateFormat.format(date);
 //		
 //		model.addAttribute("serverTime", formattedDate );
-		 //한수
+		 //한수ff
 		return "home";
 	}
 	
