7e1c7cbb325be60ee8a739cdcb8c2dc55d9bc914
leekyounghee <kyounghuiii@gmail.com>
2019-11-06 19:42:33 +0900

    test commit

---
+++ finalProject/src/main/java/com/kh/evening/board/model/exception/BoardException.java
@@ -0,0 +1,13 @@
+package com.kh.evening.board.model.exception;
+
+public class BoardException extends RuntimeException {
+
+  public BoardException() {
+    super();
+  }
+
+  public BoardException(String message) {
+    super(message);
+  }
+
+}
---
+++ finalProject/src/main/java/com/kh/evening/board/model/service/BoardService.java
@@ -0,0 +1,70 @@
+package com.kh.evening.board.model.service;
+
+import java.util.ArrayList;
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.kh.evening.board.model.dao.BoardDAO;
+import com.kh.evening.board.model.vo.Attachment;
+import com.kh.evening.board.model.vo.Board;
+import com.kh.evening.board.model.vo.PageInfo;
+
+@Service("bService")
+public class BoardService {
+  
+  @Autowired
+  private SqlSession sqlSession;
+  
+  @Autowired
+  private BoardDAO bDAO;
+
+  public ArrayList<Board> auctionList(PageInfo pi) {
+    return bDAO.auctionList(sqlSession,pi);
+  }
+
+  public int getAuctionListCount() {
+    return bDAO.getAuctionListCount(sqlSession);
+  }
+
+  public ArrayList<Board> auctionEndTimeList(PageInfo pi) {
+    return bDAO.auctionEndTimeList(sqlSession, pi);
+  }
+
+  public ArrayList<Attachment> boardFileList() {
+    return bDAO.boardFileList(sqlSession);
+  }
+
+  public int getSecondGoodListCount() {
+    return bDAO.getSecondGoodListCount(sqlSession);
+  }
+
+  public ArrayList<Board> secondGoodBoardList(PageInfo pi) {
+    return bDAO.secondGoodBoardList(sqlSession,pi);
+  }
+
+  public ArrayList<Board> auctionPriceAscList(PageInfo pi) {
+    return bDAO.auctionPriceAscList(sqlSession, pi);
+  }
+
+  public ArrayList<Board> auctionPriceDescList(PageInfo pi) {
+    return bDAO.auctionPriceDescList(sqlSession, pi);
+  }
+
+  public ArrayList<Board> secondGoodPriceAscList(PageInfo pi) {
+    return bDAO.secondGoodPriceAscList(sqlSession, pi);
+  }
+
+  public ArrayList<Board> secondGoodPriceDescList(PageInfo pi) {
+    return bDAO.secondGoodPriceDescList(sqlSession, pi);
+  }
+
+  public ArrayList<Board> auctionCountList(PageInfo pi) {
+    return bDAO.auctionCountList(sqlSession, pi);
+  }
+
+  public ArrayList<Board> secondGoodCountList(PageInfo pi) {
+    return bDAO.secondGoodCountList(sqlSession, pi);
+  }
+
+
+}
