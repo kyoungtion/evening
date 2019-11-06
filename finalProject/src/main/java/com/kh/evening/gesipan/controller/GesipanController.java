8eba415f378e5933ab881e2d938f29b7ac37829e
leekyounghee <kyounghuiii@gmail.com>
2019-11-06 19:02:44 +0900

    1106 이경희 게시글 insert 추가

--- finalProject/src/main/java/com/kh/evening/gesipan/controller/GesipanController.java
+++ finalProject/src/main/java/com/kh/evening/gesipan/controller/GesipanController.java
@@ -3,10 +3,13 @@ package com.kh.evening.gesipan.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.kh.evening.gesipan.exception.GesipanException;
 import com.kh.evening.gesipan.model.service.GesipanService;
+import com.kh.evening.gesipan.model.vo.Gesipan;
 
 @Controller
 public class GesipanController {
@@ -16,6 +19,7 @@ public class GesipanController {
 	
 	@RequestMapping("community.ge")
 	public String communityBoard() {
+		
 		return "community";
 	}
 	
@@ -33,5 +37,17 @@ public class GesipanController {
 	public String qnaInsertView(@RequestParam("g_category") String category, Model model) {
 		model.addAttribute("category", category);
 		return "gesipanInsertView";
+	}
+	
+	@RequestMapping("gInsert.ge")
+	public String gInsert(@ModelAttribute Gesipan g) {
+		
+		int result = gService.insertGesipan(g);
+		
+		if(result > 0) {
+			return "redirect:home.do";
+		} else {
+			throw new GesipanException("게시글 등록에 실패하였습니다.");
+		}
 	}
 }
--- finalProject/src/main/java/com/kh/evening/gesipan/model/dao/GesipanDAO.java
+++ finalProject/src/main/java/com/kh/evening/gesipan/model/dao/GesipanDAO.java
@@ -1,8 +1,15 @@
 package com.kh.evening.gesipan.model.dao;
 
+import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.stereotype.Repository;
+
+import com.kh.evening.gesipan.model.vo.Gesipan;
 
 @Repository("gDAO")
 public class GesipanDAO {
+
+	public int insertGesipan(SqlSessionTemplate sqlSession, Gesipan g) {
+		return sqlSession.insert("gesipanMapper.insertGesipan", g);
+	}
 
 }
