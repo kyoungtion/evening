38a2a6dc3fc40d61f12b2365b9d3deeab3cd0394
user <user@DESKTOP-KBUORPL>
2019-11-09 04:32:04 +0900

    ECHO HANDLER OKAY

--- finalProject/src/main/java/com/kh/evening/chat/controller/ChatController.java
+++ finalProject/src/main/java/com/kh/evening/chat/controller/ChatController.java
@@ -1,46 +1,40 @@
 package com.kh.evening.chat.controller;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.kh.evening.member.model.service.MemberService;
 import com.kh.evening.member.model.vo.Member;
 
 @Controller
 public class ChatController {
 
+
 	@RequestMapping("/chat.ch")
-	public ModelAndView chat(ModelAndView mv) {
-		System.out.println("=================ModelAndView===============");
-		System.out.println("=================ModelAndView===============");
-		System.out.println("=================ModelAndView===============");
-		System.out.println("=================ModelAndView===============");
-		System.out.println("=================ModelAndView===============");
-		System.out.println("=================ModelAndView===============");
+	public ModelAndView chat(HttpServletRequest request,ModelAndView mv) {
 		System.out.println("=================ModelAndView===============");
 		
-		
+		HttpSession session = request.getSession();
+        Member loginUser = (Member) session.getAttribute("loginUser");
+
+
 		
 		//사용자 정보 출력(세션)//
-		Member user = (Member) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-		if(user.equals(null)||user.equals("null")||user==null) {
-			System.out.println("=================Error===============");
-			System.out.println("=================Error===============");
-			System.out.println("=================Error===============");
-			System.out.println("=================Error===============");
-			System.out.println("=================Error===============");
-			System.out.println("=================Error===============");
-			System.out.println("=================Error===============");
-		}
-		user.setUser_name("asdf");
-		System.out.println("user name :" + user.getUser_name());
-				
-		System.out.println("normal chat page");
+/*		Member user = (Member) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+*/		
 		
-		mv.addObject("userid", user.getUser_name());
-		System.out.println("mv"+mv);
-		System.out.println("mv.getViewName()"+mv.getViewName());
+		System.out.println("loginUser      :"+loginUser);
+		System.out.println("loginUser.toString      :"+loginUser.toString());
+		mv.addObject("userid", loginUser.getNickName() );
+		mv.setViewName("chat/chattingview");
 		return mv;
 	}
 }
